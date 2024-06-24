0693cef820c49413ea4956f610be05c5db341984
180sxc <xcsplgk@gmail.com>
2024-06-24 18:05:35 +0100

    Update regavera.js

--- regavera.js
+++ regavera.js
@@ -1,16 +1,5 @@
-function getId(e){
-  return document.getElementById(e)
+function a () {
+  console.log(1)
 }
-function getClass(e){
-  return docuement.getElementsByClassName(e)
-}
-var newGame = getId("new-game")
-var openingScreen = getId("opening-screen")
-function manageGameButtons () {
-  //opening buttons
-  if(newGame) {
-    newGame.onclick = function(){
-      if(openingScreen) openingScreen.style.display = 'none'
-    }
-  }
-}
+let button = document.getElementById("new-game")
+button.onclick = a
