33f5c4887b035496da7815e9d756583166e1e71a
180sxc <xcsplgk@gmail.com>
2024-06-24 10:28:42 +0100

    Update regavera.js

--- regavera.js
+++ regavera.js
@@ -1,1 +1,11 @@
-//code here
+
+let opening= {
+  "new-game": document.getElementById("new-game"),
+  "information": document.getElementById("information"),
+  "opening-screen": document.getElemenrById("opening-screen")
+}
+function manageButtonClicks () {
+  opening["new-game"].onclick = function(){
+    opening["opening-screen"].style.display = "none"
+  }
+}
