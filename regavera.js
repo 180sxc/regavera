bed4510450417aa66ce9741d4e5b25852bafd6f5
180sxc <xcsplgk@gmail.com>
2024-06-24 23:37:51 +0100

    Update regavera.js

--- regavera.js
+++ regavera.js
@@ -1,6 +1,7 @@
 function manageDivs(){
   let newGame = document.getElementById("new-game");
   let openingScreen = document.getElementById("opening-screen")
+  openingScreen.style.backgroundImage = "url('https://i.pinimg.com/originals/08/f2/13/08f213d67bd2069184aa90dca44c720e.jpg')"
   if (newGame) {
     newGame.addEventListener("click", function(){
       openingScreen.style.display = "none"
