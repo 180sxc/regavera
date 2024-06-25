72e4217ab0c926fbc411888bc6e070752c3dd4e7
180sxc <xcsplgk@gmail.com>
2024-06-25 11:44:56 +0100

    Update regavera.js

--- regavera.js
+++ regavera.js
@@ -9,8 +9,19 @@ function loadGame(){
 }
 function manageDivs(){
   let newGame = document.getElementById("new-game");
+  let information = document.getElementById("information")
+  let settings = document.getElementById("settings")
   let openingScreen = document.getElementById("opening-screen")
   openingScreen.style.backgroundImage = "url('https://i.pinimg.com/originals/08/f2/13/08f213d67bd2069184aa90dca44c720e.jpg')"
+  setTimeout(function(){
+    newGame.style.display = "block"
+    setTimeout(function(){
+      information.style.display "block"
+      setTimeout(function(){
+        settings.style.display = "block"
+      },500)
+    },500)
+  },500)
   if (newGame) {
     newGame.addEventListener("click", function(){
       loadGame()
