ab2383452a4a36d5c821dac1f172bb7dc1e9423f
180sxc <xcsplgk@gmail.com>
2024-06-26 14:18:58 +0100

    Update regavera.js

--- regavera.js
+++ regavera.js
@@ -14,8 +14,22 @@ function manageDivs(){
   let newGame = document.getElementById("new-game");
   let information = document.getElementById("information")
   let openingScreen = document.getElementById("opening-screen")
+  let settings = document.getElementById("settings")
   openingScreen.style.backgroundImage = "url('https://art.ngfiles.com/images/1525000/1525565_baukjespirit_pixel-castle-landscape.png?f1606737195')";
+  if(settings) {
+    setTimeout(()=>{
+      settings.style.display = "block"
+    },1500)
+  }
+  if(information){
+    setTimeout(()=>{
+      information.style.display = "block"
+    },1000)
+  }
   if (newGame) {
+    setTimeout(()=>{
+      newGame.style.display = "block"
+    },500)
     newGame.addEventListener("click", function(){
       loadGame()
     });
